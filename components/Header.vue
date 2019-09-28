0b30de973e982a4f78f02bed1a3c686d00498923
EGOIST <0x142857@gmail.com>
2019-09-28 18:32:30 +0800

    UI tweaks

--- components/Header.vue
+++ components/Header.vue
@@ -3,14 +3,26 @@
     <div class="container">
       <div class="header-left">
         <h1 class="site-title">
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 280 348">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" viewBox="0 0 280 371">
             <g fill="none" fill-rule="evenodd">
-              <polygon fill="#E1E7FA" points="0 0 140 0 280 137 0 137" />
-              <polygon
-                fill="#5D64FF"
-                points="0 0 140 0 280 140 140 140 140 280 0 139.489"
-                transform="translate(0 68)"
+              <path
+                fill="#F15C47"
+                d="M37.5,296 C58.2106781,296 75,312.789322 75,333.5 C75,354.210678 58.2106781,371 37.5,371 C16.7893219,371 2.53632657e-15,354.210678 0,333.5 C-2.53632657e-15,312.789322 16.7893219,296 37.5,296 Z"
               />
+              <path
+                fill="#FFC40C"
+                d="M37.5,0 L242.5,0 C263.210678,-3.80448985e-15 280,16.7893219 280,37.5 C280,58.2106781 263.210678,75 242.5,75 L37.5,75 C16.7893219,75 2.53632657e-15,58.2106781 0,37.5 C-2.53632657e-15,16.7893219 16.7893219,3.80448985e-15 37.5,0 Z"
+              />
+              <g transform="translate(0 148)">
+                <path
+                  fill="#3EBAEC"
+                  d="M37.5,0 L112.5,0 C133.210678,-3.80448985e-15 150,16.7893219 150,37.5 C150,58.2106781 133.210678,75 112.5,75 L37.5,75 C16.7893219,75 2.53632657e-15,58.2106781 0,37.5 C-2.53632657e-15,16.7893219 16.7893219,3.80448985e-15 37.5,0 Z"
+                />
+                <path
+                  fill="#3C449C"
+                  d="M37.5,0 C58.2106781,-3.80448985e-15 75,16.7893219 75,37.5 C75,58.2106781 58.2106781,75 37.5,75 C16.7893219,75 2.53632657e-15,58.2106781 0,37.5 C-2.53632657e-15,16.7893219 16.7893219,3.80448985e-15 37.5,0 Z"
+                />
+              </g>
             </g>
           </svg>
           <saber-link to="/">{{ $siteConfig.title }}</saber-link>
@@ -29,8 +41,7 @@ export default Vue.extend({})
 
 <style scoped>
 .header {
-  border-bottom: 1px solid #ebecf1;
-  height: var(--header-height);
+  padding: 60px 0;
 
   & > .container {
     display: flex;
@@ -49,7 +60,7 @@ export default Vue.extend({})
   font-weight: 700;
 
   & svg {
-    margin-right: 10px;
+    margin-right: 12px;
   }
 }
 
